4e6098ceb7c03e87b2452a2a924f487ae4386181
Nersesk <knersesk@gmail.com>
2021-10-31 19:45:46 +0300

    added News

---
+++ fan_page/news/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import main_page, show_post, show_category
+
+urlpatterns = [
+    path('',main_page,name='Home' ),
+    path('post/<slug:post_slug>/', show_post,name='post' ),
+    path('category/<slug:category_slug>/',show_category,name='category'),
+]
--- fan_page/news/views.py
+++ fan_page/news/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
-# Create your views here.
+
+def main_page(requset):
+    menu = {
+
+        'News': 'News',
+        'Shop': 'Shop',
+        'Contact us': 'Contact us',
+        'About us': 'About us',
+        'Log In': 'Log In'
+    }
+    category = Category.objects.all()
+    post = Post.objects.all()
+
+    return render(requset, 'news/intro.html', {'menu': menu, 'category': category, 'post': post,'title':'F.C.B'})
+
+
+def show_post(request, post_slug):
+    menu = {
+        'News': 'News',
+        'Shop': 'Shop',
+        'Contact us': 'Contact us',
+        'About us': 'About us',
+        'Log In': 'Log In'
+    }
+    post = get_object_or_404(Post, slug=post_slug)
+    category = Category.objects.all()
+    return render(request, 'news/post.html', {'menu': menu, 'post': post, 'category': category})
+
+
+def show_category(requset, category_slug):
+    menu = {
+        'News': 'News',
+        'Shop': 'Shop',
+        'Contact us': 'Contact us',
+        'About us': 'About us',
+        'Log In': 'Log In'
+    }
+    category = Category.objects.all()
+    post = Post.objects.filter(cat__slug=category_slug)
+
+    return render(requset, 'news/show_category.html', {'menu': menu, 'category': category, 'post': post})
